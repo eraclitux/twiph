e018ce4f5a1c19425670ae5e13573bcd8fe91075
Andrea Masi <eraclitux@gmail.com>
2015-10-21 08:32:11 +0200

    Cross compile for darwin amd64

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ BTIME=`date -u +%s`
 go build -ldflags "-w -X main.Version=${GIT_TAG} -X main.BuildTime=${BTIME}" -o ./build/bin/twiph
 cp -r ./templates ./build
 tar -czvf /tmp/linux_X86-64.tar.gz ./build
+# Build for Mac OSX
+env GOOS=darwin GOARCH=amd64 go build -ldflags "-w -X main.Version=${GIT_TAG} -X main.BuildTime=${BTIME}" -o ./build/bin/twiph
+tar -czvf /tmp/darwin_X86-64.tar.gz ./build
